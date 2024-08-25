306ebe50e62c0d95e4ebe3636348b013ad06f275
Debiprasad2002 <debiprasadbedanta@gmail.com>
2024-08-25 23:23:21 +0530

    update the files with recent changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,8 +5,7 @@ function About() {
     return (
         <div className="about">
             <h1>About Us</h1>
-            <p>Nexinfluence is a leader in influencer marketing, helping brands connect with influencers to maximize their reach.Nexinfluence is a leader in influencer marketing, helping brands connect with influencers to maximize their reachNexinfluence is a leader in influencer marketing, helping brands connect with influencers to maximize their reach</p>
-            <p>Our mission is to create lasting partnerships between brands and influencers.</p>
+            <p>Nexinfluence is a leader in influencer marketing, helping brands connect with influencers to maximize their reach.</p>
         </div>
     );
 }
